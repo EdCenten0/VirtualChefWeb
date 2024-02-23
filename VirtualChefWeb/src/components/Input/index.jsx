041c59eb797841f1ae4124f1d4be5832506371ae
Ed Centeno <cchavarriacenteno8@gmail.com>
2024-02-22 22:32:44 -0600

    Update: botones de agregar y eliminar
    Estos botones estan pensados para la pantalla de crear receta

--- VirtualChefWeb/src/components/Input/index.jsx
+++ VirtualChefWeb/src/components/Input/index.jsx
@@ -9,14 +9,35 @@
 // Yap, eso es todo, espero que te sirva, saludos!
 
 import cross from "./cross-green.svg";
+import trash from "./trash.svg";
 
-const Input = ({ name, placeholder, addIcon }) => {
+const Input = ({ name, placeholder, addIcon, deleteIcon }) => {
   const renderAddIcon = () => {
     if (addIcon) {
       return (
         <>
-          <div className='w-12 h-full flex items-center justify-center bg-white border border-black'>
-            <img className='rotate-45 w-7' src={cross} alt='cross' />
+          <div className='w-12 h-full flex items-center justify-center bg-white border border-black rounded-lg cursor-pointer'>
+            <img
+              className='rotate-45 w-7 cursor-pointer'
+              src={cross}
+              alt='cross'
+            />
+          </div>
+        </>
+      );
+    }
+  };
+
+  const renderDeleteIcon = () => {
+    if (deleteIcon) {
+      return (
+        <>
+          <div className='w-12 h-full flex items-center justify-center bg-white border border-black rounded-lg cursor-pointer'>
+            <img
+              className='w-7 h-full cursor-pointer'
+              src={trash}
+              alt='cross'
+            />
           </div>
         </>
       );
@@ -37,7 +58,8 @@ const Input = ({ name, placeholder, addIcon }) => {
             className='m-0 outline-black border-black rounded-[5px] border font-light w-full px-[.4rem] py-[.6rem] placeholder:text-gray-400 placeholder:font-thin'
           />
         </div>
-        <div className='h-full'>{renderAddIcon()}</div>
+        {renderAddIcon()}
+        {renderDeleteIcon()}
       </div>
     </>
   );
