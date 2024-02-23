0c6f7cf4d3772a151a8bc0bffe8dff75847383c7
Ed Centeno <cchavarriacenteno8@gmail.com>
2024-02-22 22:37:12 -0600

    Update index.jsx

--- VirtualChefWeb/src/pages/Menu_Principal/index.jsx
+++ VirtualChefWeb/src/pages/Menu_Principal/index.jsx
@@ -2,13 +2,13 @@
 import Header from "../../components/Header/index";
 import InputIcon from "../../components/Input/InputIcon";
 import SearchIcon from "../../assets/search.jpg";
-import CarruselComidas from "../../components/carrusel/CarruselComidas";
+import CarruselComidas from "../../components/Carrusel/CarruselComidas";
 
 function Menu_Principal() {
   return (
     <>
-      <main className="w-screen p-10 flex justify-center ">
-        <div className=" h-screen w-screen max-w-[1200px]">
+      <main className='w-screen p-10 flex justify-center '>
+        <div className=' h-screen w-screen max-w-[1200px]'>
           <Header></Header>
           <InputIcon
             icon={SearchIcon}
@@ -25,7 +25,6 @@ function Menu_Principal() {
           <CarruselComidas name={"Cena"}></CarruselComidas>
 
           {/*Footer*/}
-
         </div>
       </main>
     </>
