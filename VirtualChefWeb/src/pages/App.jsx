586bb11a80fedd2a12f305254b5e68aef936f3cd
Ed Centeno <cchavarriacenteno8@gmail.com>
2024-02-22 22:40:11 -0600

    Update: Rutas agregadas
    He agregado rutas al react router dom

--- VirtualChefWeb/src/pages/App.jsx
+++ VirtualChefWeb/src/pages/App.jsx
@@ -1,4 +1,4 @@
-import { CrearReceta1, CrearReceta2 } from "./CrearReceta";
+import { CrearReceta1, CrearReceta2, CrearReceta3 } from "./CrearReceta";
 import { Registrar } from "./Login/Registrar";
 import { useRoutes, BrowserRouter } from "react-router-dom";
 import Login from "./Login/index";
@@ -7,13 +7,17 @@ import Menu_Principal from "./Menu_Principal";
 function App() {
   const AppRoutes = () => {
     let routes = useRoutes([
+      { path: "*", element: <Login /> },
       {
-        path: "/login",
+        path: "/",
         element: <Login />,
       },
       { path: "/favoritos", element: <Login /> },
       { path: "/registrar", element: <Registrar /> },
       { path: "/home", element: <Menu_Principal /> },
+      { path: "/crearReceta1", element: <CrearReceta1 /> },
+      { path: "/crearReceta2", element: <CrearReceta2 /> },
+      { path: "/crearReceta3", element: <CrearReceta3 /> },
       { path: "/about" },
     ]);
 
