ea5b9765bc435f62a73cc1c0385e1ba16a55f520
Ed Centeno <cchavarriacenteno8@gmail.com>
2024-02-22 18:51:41 -0600

    Update:  react router dom agregado
    La libreria ha sido agregado a grandes rasgos

--- VirtualChefWeb/src/pages/Login/Registrar.jsx
+++ VirtualChefWeb/src/pages/Login/Registrar.jsx
@@ -3,13 +3,13 @@ import InputCom from "../../components/Input";
 import ButtonCom from "../../components/Button";
 import Logo from "../../assets/Logo.svg";
 
-const Registar = () => {
+const Registrar = () => {
   return (
     <>
-      <div className=" gap-5 h-screen flex flex-col items-center justify-center p-14">
-        <img src={Logo} className="size-[250px]" />
-        <form className="w-[500px]">
-          <div className="mb-5">
+      <div className=' gap-5 h-screen flex flex-col items-center justify-center p-14'>
+        <img src={Logo} className='size-[250px]' />
+        <form className='w-[500px]'>
+          <div className='mb-5'>
             <InputCom name={"Correo electronico"}></InputCom>
             <InputCom name={"Nombre"}></InputCom>
             <InputCom name={"Apellido"}></InputCom>
@@ -24,4 +24,4 @@ const Registar = () => {
   );
 };
 
-export default Registar;
+export { Registrar };
