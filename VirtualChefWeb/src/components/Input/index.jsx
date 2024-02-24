700fcd24726c77254fb0e2d9048322581bedf025
Ed Centeno <cchavarriacenteno8@gmail.com>
2024-02-24 17:15:06 -0600

    Update: Cambios variados
    Cambios no especificos

--- VirtualChefWeb/src/components/Input/index.jsx
+++ VirtualChefWeb/src/components/Input/index.jsx
@@ -1,17 +1,6 @@
-// De Carlos para Carlos:
-// En el caso de las paginas donde el input es un text area, podria hacer que este
-// componente sea mas generico y pueda recibir un parametro que tenga las siguientes posibilidades:
-// 1: En un render conditional podria generar un input o un text area
-// 2: Aumento el tamaño del input para que se convierta en un textarea
-// Otra cosa que podria hacer es un componente aparte que sea un textarea, pero siento que se puede evitar y yap
-// Por otro lado, se me ocurre que para hacerlo mas responsive podria hacer que por defecto el width y height sean del 100%
-// y asi hacer que el componente solamente necesite estar dentro de una etiqueta padre que va a ser la que determine el tamaño del componente
-// Yap, eso es todo, espero que te sirva, saludos!
-
 import cross from "./cross-green.svg";
-import trash from "./trash.svg";
 
-const Input = ({ name, placeholder, addIcon, deleteIcon }) => {
+const Input = ({ name, placeholder, addIcon }) => {
   const renderAddIcon = () => {
     if (addIcon) {
       return (
@@ -20,22 +9,6 @@ const Input = ({ name, placeholder, addIcon, deleteIcon }) => {
             <img
               className='rotate-45 w-7 cursor-pointer'
               src={cross}
-              alt='cross'
-            />
-          </div>
-        </>
-      );
-    }
-  };
-
-  const renderDeleteIcon = () => {
-    if (deleteIcon) {
-      return (
-        <>
-          <div className='w-12 h-full flex items-center justify-center bg-white border border-black rounded-lg cursor-pointer'>
-            <img
-              className='w-7 h-full cursor-pointer'
-              src={trash}
               alt='cross'
             />
           </div>
@@ -59,7 +32,6 @@ const Input = ({ name, placeholder, addIcon, deleteIcon }) => {
           />
         </div>
         {renderAddIcon()}
-        {renderDeleteIcon()}
       </div>
     </>
   );
