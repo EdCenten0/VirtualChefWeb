37a155ce21fdae8cce625686cad5c792f0a61748
FranciscoMelen10 <francisco.melendez14454@est.uca.edu.ni>
2024-02-25 00:32:40 -0600

    Add: Pop_button
    Botón reutilizable que se puede agregar una imagen y texto

--- VirtualChefWeb/src/pages/Menu_Principal/index.jsx
+++ VirtualChefWeb/src/pages/Menu_Principal/index.jsx
@@ -3,11 +3,16 @@ import Header from "../../components/Header/index";
 import InputIcon from "../../components/Input/InputIcon";
 import SearchIcon from "../../assets/search.jpg";
 import CarruselComidas from "../../components/Carrusel/CarruselComidas";
+import Pop_button from '../../components/Button/Pop_button';
+import IconCreate from '../../assets/Create-pencil.svg'
 
 function Menu_Principal() {
   return (
     <>
       <main className='w-screen p-10 flex justify-center '>
+        <div className="fixed bottom-0 left-0 p-5 z-20">
+          <Pop_button Icon={IconCreate} text={"Crear"}></Pop_button>
+        </div>
         <div className=' h-screen w-screen max-w-[1200px]'>
           <Header></Header>
           <InputIcon
