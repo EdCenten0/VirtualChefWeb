1ce1bbe4e3fe44ae10572f7a82c15a822533bc8e
Ed Centeno <cchavarriacenteno8@gmail.com>
2024-02-24 21:29:48 -0600

    Update: Al presionar el logo lleva al home

--- VirtualChefWeb/src/components/Header/index.jsx
+++ VirtualChefWeb/src/components/Header/index.jsx
@@ -5,12 +5,28 @@ const Header = () => {
   return (
     <>
       <header>
-        <nav className="w-full flex items-center justify-around">
-          <NavLink to="/home" className="hover:cursor-pointer hover:underline">Home</NavLink>
-          <NavLink to="/favoritos" className="hover:cursor-pointer hover:underline">Favoritos</NavLink>
-          <img src={Logo} className="size-[100px]" />
-          <NavLink to="/recetas" className="hover:cursor-pointer hover:underline">Recetas</NavLink>
-          <NavLink to="/about" className="hover:cursor-pointer hover:underline">Acerca de</NavLink>
+        <nav className='w-full flex items-center justify-around'>
+          <NavLink to='/home' className='hover:cursor-pointer hover:underline'>
+            Home
+          </NavLink>
+          <NavLink
+            to='/favoritos'
+            className='hover:cursor-pointer hover:underline'
+          >
+            Favoritos
+          </NavLink>
+          <NavLink to='/home' className='hover:cursor-pointer hover:underline'>
+            <img src={Logo} className='size-[100px]' />
+          </NavLink>
+          <NavLink
+            to='/recetas'
+            className='hover:cursor-pointer hover:underline'
+          >
+            Recetas
+          </NavLink>
+          <NavLink to='/about' className='hover:cursor-pointer hover:underline'>
+            Acerca de
+          </NavLink>
         </nav>
       </header>
     </>
