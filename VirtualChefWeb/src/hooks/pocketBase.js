20a5120c5eff5cd63360c58e60ac62804a878837
Ed Centeno <cchavarriacenteno8@gmail.com>
2024-03-08 09:36:14 -0600

    Add: Pocketbase como base de datos local

---
+++ VirtualChefWeb/src/hooks/pocketBase.js
@@ -0,0 +1,12 @@
+import PocketBase from "pocketbase";
+
+const pb = new PocketBase("http://127.0.0.1:8090");
+
+const records = await pb.collection("users").getFullList({});
+
+const adminData = await pb.admins.authWithPassword(
+  "cchavarriacenteno8@gmail.com",
+  "dsadasdasd"
+);
+
+export { pb, records };
--- VirtualChefWeb/src/pages/Login/index.jsx
+++ VirtualChefWeb/src/pages/Login/index.jsx
@@ -4,6 +4,7 @@ import ButtonCom from "../../components/Button";
 import { useForm } from "react-hook-form";
 import Logo from "../../assets/Logo.svg";
 import { NavLink } from "react-router-dom";
+import { pb, records } from "../../hooks/pocketBase";
 
 const Login = () => {
   const {
@@ -11,6 +12,9 @@ const Login = () => {
     handleSubmit,
     formState: { errors },
   } = useForm();
+
+  console.log(records);
+
   return (
     <>
       <div className='w-screen h-screen gap-3 flex flex-col items-center justify-center '>
