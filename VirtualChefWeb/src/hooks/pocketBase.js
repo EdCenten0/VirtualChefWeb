9ed6e4857f5333739c2a887f81a4e1c42e2a414a
Ed Centeno <cchavarriacenteno8@gmail.com>
2024-03-08 10:31:02 -0600

    Update pocketBase.js

--- VirtualChefWeb/src/hooks/pocketBase.js
+++ VirtualChefWeb/src/hooks/pocketBase.js
@@ -4,6 +4,8 @@ const pb = new PocketBase("http://127.0.0.1:8090");
 
 const records = await pb.collection("users").getFullList({});
 
+// Contraseña: casimiro1234
+
 const adminData = await pb.admins.authWithPassword(
   "cchavarriacenteno8@gmail.com",
   "dsadasdasd"
