ef74498ed8a038dbdff143f6a9755a5e1941da7c
Ed Centeno <cchavarriacenteno8@gmail.com>
2024-03-08 11:02:34 -0600

    Update: Usuario administrador generico creado

--- VirtualChefWeb/src/hooks/pocketBase.js
+++ VirtualChefWeb/src/hooks/pocketBase.js
@@ -4,8 +4,9 @@ const pb = new PocketBase("http://127.0.0.1:8090");
 
 const records = await pb.collection("users").getFullList({});
 
-// Contraseña: casimiro1234
-// Prueba de autenticación
+//  Credenciales
+//  Correo: admin@gmail.com
+//  password: admin123456
 
 const adminData = await pb.admins.authWithPassword(
   "cchavarriacenteno8@gmail.com",
