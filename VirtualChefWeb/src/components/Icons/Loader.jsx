9497bdee4dfaa763f952147c40f1be2351990a6c
FranciscoMelen10 <fmelendezsimplina@gmail.com>
2024-03-24 14:13:44 -0600

    Add: Loader

---
+++ VirtualChefWeb/src/components/Icons/Loader.jsx
@@ -0,0 +1,9 @@
+export default function Loader() {
+  return (
+    <div className="flex flex-row gap-2 size-full justify-center items-center ">
+      <div className="w-4 h-4 rounded-full bg-[#246C2C] animate-bounce"></div>
+      <div className="w-4 h-4 rounded-full bg-[#246C2C] animate-bounce [animation-delay:-.3s]"></div>
+      <div className="w-4 h-4 rounded-full bg-[#246C2C] animate-bounce [animation-delay:-.5s] "></div>
+    </div>
+  );
+}
