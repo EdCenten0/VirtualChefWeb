8597582458239b4036c06f2290c3d64a11bd6f0c
FranciscoMelen10 <fmelendezsimplina@gmail.com>
2024-03-30 16:09:58 -0600

    Update: Agregue "Lazy Load" en el router de todas las páginas

--- VirtualChefWeb/src/pages/App.jsx
+++ VirtualChefWeb/src/pages/App.jsx
@@ -1,15 +1,24 @@
+import { useRoutes, BrowserRouter } from "react-router-dom";
+import { Suspense, lazy } from "react";
+
+// Páginas
 import { CrearReceta1, CrearReceta2, CrearReceta3 } from "./CrearReceta";
 import { Registrar } from "./Login/Registrar";
-import { useRoutes, BrowserRouter } from "react-router-dom";
-import Login from "./Login/index";
-import NotFound from "./Not_Found/Not_Found";
-import Menu_Principal from "./Menu_Principal";
-import About from "./About";
-import Favoritos from "./Favoritos/index";
-import VistaReceta from "./VIstaReceta";
+const Login = lazy(() => import("./Login/index"));
+const NotFound = lazy(() => import("./Not_Found/Not_Found"));
+const Menu_Principal = lazy(() => import("./Menu_Principal"));
+const About = lazy(() => import("./About"));
+const Favoritos = lazy(() => import("./Favoritos/index"));
+const VistaReceta = lazy(() => import("./VIstaReceta"));
+const Perfil = lazy(() => import("./Perfil/index"));
+const Buscar = lazy(() => import("./Buscar/index"));
+
+// Componentes
+import Loader from "../components/Icons/Loader";
+
+//Contextos 
 import { UserProvider } from "../contexts/UserContext";
 import { CrearRecetaProvider } from "../contexts/CrearRecetaContext";
-import Perfil from "./Perfil/index"; 
 
 function App() {
   const AppRoutes = () => {
@@ -25,6 +34,7 @@ function App() {
       { path: "/receta", element: <VistaReceta /> },
       { path: "/about", element: <About /> },
       { path: "/perfil", element: <Perfil /> },
+      { path: "/buscar", element: <Buscar /> },
     ]);
 
     return routes;
@@ -34,9 +44,17 @@ function App() {
     <>
       <UserProvider>
         <CrearRecetaProvider>
-          <BrowserRouter>
-            <AppRoutes />
-          </BrowserRouter>
+          <Suspense
+            fallback={
+              <div className="w-screen h-screen flex justify-center items-center">
+                <Loader></Loader>
+              </div>
+            }
+          >
+            <BrowserRouter>
+              <AppRoutes />
+            </BrowserRouter>
+          </Suspense>
         </CrearRecetaProvider>
       </UserProvider>
     </>
