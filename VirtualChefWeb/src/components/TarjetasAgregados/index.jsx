1d73235d6b8ad8d0eb27e1de82fe44e87e4f79dd
Ed Centeno <cchavarriacenteno8@gmail.com>
2024-04-01 20:54:09 -0600

    Update: Las tarjetas de agregados han sido codificadas
    Pueden ser usadas tanto para los ingredientes o ya sea para los pasos, ya que se han hecho de forma generica. Tambien son capaces de eliminar los elementos dentro de sus respectivos array

--- VirtualChefWeb/src/components/TarjetasAgregados/index.jsx
+++ VirtualChefWeb/src/components/TarjetasAgregados/index.jsx
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { CrearRecetaContext } from "../../contexts/CrearRecetaContext";
 import trash from "./trash.svg";
 
-const TarjetasAgregados = () => {
+const TarjetasAgregados = ({ elemento, nombreDelArray }) => {
+  const { receta, setReceta } = useContext(CrearRecetaContext);
+
+  const deleteElemento = () => {
+    const newArray = receta[nombreDelArray].filter((elem) => elem !== elemento);
+
+    setReceta({
+      ...receta,
+      [nombreDelArray]: newArray,
+    });
+  };
+
   return (
     <div className='flex items-center justify-center gap-3 my-2'>
       <div className='bg-white border border-black p-2 w-full rounded-lg'>
-        <p>213123123233213213</p>
+        <p>{elemento}</p>
       </div>
-      <div className='w-12 h-full flex items-center justify-center bg-white border border-black rounded-lg cursor-pointer'>
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          deleteElemento();
+        }}
+        className='w-12 h-full flex items-center justify-center bg-white border border-black rounded-lg cursor-pointer'
+      >
         <img className='w-7 h-10 cursor-pointer' src={trash} alt='cross' />
-      </div>
+      </button>
     </div>
   );
 };
