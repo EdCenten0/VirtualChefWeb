7ae05d867ec97c9ff98c0fc6de0b67625df35ea8
Ed Centeno <cchavarriacenteno8@gmail.com>
2024-04-07 18:57:31 -0600

    Update: En la base de datos ahora las recetas aceptan SVG

---
+++ database/pocketbase_0.22.3_windows_amd64/pb_migrations/1712537812_updated_recetas.js
@@ -0,0 +1,56 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("fu8m3c9ssj7wzsl")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "m9mtvcyq",
+    "name": "imagen",
+    "type": "file",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "mimeTypes": [
+        "image/jpeg",
+        "image/png",
+        "image/vnd.mozilla.apng",
+        "image/svg+xml"
+      ],
+      "thumbs": [],
+      "maxSelect": 1,
+      "maxSize": 5242880,
+      "protected": false
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("fu8m3c9ssj7wzsl")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "m9mtvcyq",
+    "name": "imagen",
+    "type": "file",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "mimeTypes": [
+        "image/jpeg",
+        "image/png"
+      ],
+      "thumbs": [],
+      "maxSelect": 1,
+      "maxSize": 5242880,
+      "protected": false
+    }
+  }))
+
+  return dao.saveCollection(collection)
+})
