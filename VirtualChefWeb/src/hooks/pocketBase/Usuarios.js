eee4857eb410a234af38c6cf342ac81532118687
Ed Centeno <cchavarriacenteno8@gmail.com>
2024-04-09 23:27:07 -0600

    Update: Funcion para buscar la info de un usuario para asi mostrarla en el perfil

--- VirtualChefWeb/src/hooks/pocketBase/Usuarios.js
+++ VirtualChefWeb/src/hooks/pocketBase/Usuarios.js
@@ -8,69 +8,83 @@ const ALL_USERS = await pb.collection("users").getFullList({});
 pb.autoCancellation(false);
 
 export async function createUser(data) {
-
-    // Buscar el ID del rol de "usuario"
-    const idRol = await pb.collection("roles").getFullList({}, {
-        filter: `nombre = "usuario"`,
-    });
-
-    try {
-        // Tabla de usuarios en pocketbase
-        const datos = {
-            username: data.nombre_de_usuario,
-            email: data.correo_electronico,
-            emailVisibility: true,
-            password: data.contraseña,
-            passwordConfirm: data.confirmar_contraseña,
-            nombre: data.nombre,
-            apellido: data.apellido,
-            rolID: idRol[0].id// ID del rol de usuario
-        }
-
-        // Comprueba si hay un usuario con el mismo correo o nombre de usuario
-        const user = await existeUsuario(datos.email, datos.username)
-
-        // la varible "user" es un arreglo con los usuarios que coinciden con el correo o nombre de usuario
-        if (user.length === 1) {
-            alert("Usuario ya existe, intente con otro correo o nombre de usuario");
-        } else {
-            // Crea el usuario, porque no existe
-            await pb.collection('users').create(datos);
-            alert("Usuario creado con exito");
-        }
-
-        return user;
-
-    } catch (error) {
-        alert(error);
+  // Buscar el ID del rol de "usuario"
+  const idRol = await pb.collection("roles").getFullList(
+    {},
+    {
+      filter: `nombre = "usuario"`,
     }
+  );
+
+  try {
+    // Tabla de usuarios en pocketbase
+    const datos = {
+      username: data.nombre_de_usuario,
+      email: data.correo_electronico,
+      emailVisibility: true,
+      password: data.contraseña,
+      passwordConfirm: data.confirmar_contraseña,
+      nombre: data.nombre,
+      apellido: data.apellido,
+      rolID: idRol[0].id, // ID del rol de usuario
+    };
+
+    // Comprueba si hay un usuario con el mismo correo o nombre de usuario
+    const user = await existeUsuario(datos.email, datos.username);
+
+    // la varible "user" es un arreglo con los usuarios que coinciden con el correo o nombre de usuario
+    if (user.length === 1) {
+      alert("Usuario ya existe, intente con otro correo o nombre de usuario");
+    } else {
+      // Crea el usuario, porque no existe
+      await pb.collection("users").create(datos);
+      alert("Usuario creado con exito");
+    }
+
+    return user;
+  } catch (error) {
+    alert(error);
+  }
 }
 
 async function loginUsuario(email, password) {
-    try {
-        const authData = await pb.collection('users').authWithPassword(`${email}`, `${password}`);
-        return authData;
-    } catch (error) {
-        console.log(error);
-        alert("Usuario no encontrado, intente de nuevo");
-    }
+  try {
+    const authData = await pb
+      .collection("users")
+      .authWithPassword(`${email}`, `${password}`);
+    return authData;
+  } catch (error) {
+    console.log(error);
+    alert("Usuario no encontrado, intente de nuevo");
+  }
 }
 
 async function existeUsuario(email, username, id) {
-    try {
-        // Busca si hay un usuario con el mismo correo o nombre de usuario
-        const user = await pb.collection("users").getFullList({}, {
-            filter: `email = "${email}" || username = "${username}" || id = "${id}"`,
-        });
+  try {
+    // Busca si hay un usuario con el mismo correo o nombre de usuario
+    const user = await pb.collection("users").getFullList(
+      {},
+      {
+        filter: `email = "${email}" || username = "${username}" || id = "${id}"`,
+      }
+    );
 
-        // Devuelve un arreglo con los usuarios que coinciden con el correo o nombre de usuario
-        return user;
-
-    } catch (error) {
-        console.log(error);
-    }
+    // Devuelve un arreglo con los usuarios que coinciden con el correo o nombre de usuario
+    return user;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
+async function buscarInfoUsuario(id) {
+  try {
+    const user = await pb
+      .collection("users")
+      .getFullList({ filter: `id = "${id}"` });
+    return user;
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-
-export { ALL_USERS, loginUsuario, existeUsuario };
+export { ALL_USERS, loginUsuario, existeUsuario, buscarInfoUsuario };
