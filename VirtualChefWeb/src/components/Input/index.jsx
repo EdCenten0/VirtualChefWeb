904c73da5ccf3fa0004c067ab2ff3bd279ddf3d8
Ed Centeno <cchavarriacenteno8@gmail.com>
2024-04-14 00:39:43 -0600

    Update:  pequeños cambios en el input para aceptar estilos custom

--- VirtualChefWeb/src/components/Input/index.jsx
+++ VirtualChefWeb/src/components/Input/index.jsx
@@ -3,18 +3,22 @@ const Input = ({
   placeholder,
   register = () => {},
   isRequired = false,
-  errors,
+  errors = {},
   type = "text",
   minLength = 10,
+  containerClassName = "",
+  inputClassName = "",
 }) => {
   const replaceName = name.replace(/ /g, "_").toLowerCase();
 
   return (
     <>
-      <div className='relative my-[1rem] flex gap-3 items-center'>
+      <div
+        className={`${containerClassName} relative my-[1rem] flex gap-3 items-center`}
+      >
         <div className='w-full'>
           <p
-            className={`py-0 px-1 m-0 absolute -top-[12px] left-[10px] bg-white font-extralight text-[0.9rem] rounded-[5px]`}
+            className={` py-0 px-1 m-0 absolute -top-[12px] left-[10px] bg-white font-extralight text-[0.9rem] rounded-[5px]`}
           >
             {name}
           </p>
@@ -24,7 +28,7 @@ const Input = ({
               minLength: minLength,
             })}
             placeholder={placeholder}
-            className='m-0 outline-black border-black rounded-[5px] border font-light w-full px-[.4rem] py-[.6rem] placeholder:text-gray-400 placeholder:font-thin'
+            className={`${inputClassName} m-0 outline-black border-black rounded-[5px] border font-light w-full px-[.4rem] py-[.6rem] placeholder:text-gray-400 placeholder:font-thin`}
             type={`${type}`}
           />
           {errors[replaceName] && (
