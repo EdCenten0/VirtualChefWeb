cedf76875464e8d9bff457870f2f7d2009b1eb7f
Ed Centeno <cchavarriacenteno8@gmail.com>
2024-04-23 19:51:49 -0600

    Update: Actualizacion en about

--- VirtualChefWeb/src/pages/About/index.jsx
+++ VirtualChefWeb/src/pages/About/index.jsx
@@ -28,31 +28,31 @@ const About = () => {
   if (!isLoading) {
     return userIsValid ? (
       <>
-        <div className="w-screen p-10 flex justify-center ">
-          <div className="w-screen flex flex-col  max-w-[1200px]">
+        <div className='w-screen p-10 flex justify-center '>
+          <div className='w-screen flex flex-col  max-w-[1200px]'>
             <Header />
           </div>
         </div>
 
-        <main className="flex justify-center items-center flex-col m-10">
-          <section className="bg-slate-100 w-[99vw] p-10 ">
-            <div className="w-full">
-              <h2 className="text-center font-bold text-xl my-4">
+        <main className='flex justify-center items-center flex-col m-10'>
+          <section className='bg-slate-100 w-[99vw] p-10 '>
+            <div className='w-full'>
+              <h2 className='text-center font-bold text-xl my-4'>
                 ¿Qué es Virtual Chef?
               </h2>
-              <p className="my-2 text-center font-normal">
+              <p className='my-2 text-center font-normal'>
                 Virtual Chef es una aplicación web que te permite buscar recetas
                 de comida, guardar tus recetas favoritas, y crear tus propias
                 recetas.
               </p>
             </div>
           </section>
-          <section className=" bg-slate-200 w-[99vw] pb-20">
-            <div className="">
-              <h2 className="text-center font-bold text-xl my-4">
+          <section className=' bg-slate-200 w-[99vw] pb-20'>
+            <div className=''>
+              <h2 className='text-center font-bold text-xl my-4'>
                 ¿Quiénes somos?
               </h2>
-              <div className="flex gap-3 justify-around">
+              <div className='flex gap-3 justify-around'>
                 <ProfileCard
                   pattern={1}
                   nombre={"Isabel Denisse Aguilar Vilchez"}
@@ -79,15 +79,6 @@ const About = () => {
                     "https://avatars.githubusercontent.com/u/104796963?v=4"
                   }
                   link={"https://github.com/FranciscoMelen10"}
-                />
-                <ProfileCard
-                  pattern={4}
-                  nombre={"Ana Marbell Zepeda Alméndarez"}
-                  descripcion={"Web developer"}
-                  imgUrl={
-                    "https://avatars.githubusercontent.com/u/101136508?v=4"
-                  }
-                  link={"https://github.com/marbell04"}
                 />
               </div>
             </div>
